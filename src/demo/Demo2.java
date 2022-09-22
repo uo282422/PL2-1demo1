707b04282a2baae72803849861749fe41a2a5076
Usuario <Usuario@DESKTOP-2CI8FOC>
2022-09-22 13:24:46 +0200

    Commit en rama principal de Demo1 y Demo2

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -4,8 +4,7 @@ public class Demo2 {
 		public static void main(String[] args) {
 		
 			System.out.println("Prueba 1");
-			System.out.println("Prueba 1");
-			System.out.println("Prueba 1");
-			System.out.println("Nuevo elemento 1");
+			System.out.println("Prueba 2");
+			System.out.println("Prueba 3");
 		}
 }
