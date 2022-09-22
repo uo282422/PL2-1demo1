e127e2bd163f08f3a0ad5087b339eca28b7d1acc
Usuario <Usuario@DESKTOP-2CI8FOC>
2022-09-22 13:19:35 +0200

    Commit con tres lineas de prueba

--- src/demo/Demo1.java
+++ src/demo/Demo1.java
@@ -2,6 +2,9 @@ package demo;
 
 public class Demo1 {
 		public static void main(String[] args) {
+		
+			System.out.println("Prueba 1");
+			System.out.println("Prueba 1");
 			System.out.println("Prueba 1");
 		}
 }
